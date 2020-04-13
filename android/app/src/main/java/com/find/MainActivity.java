7777febb4d4807a657ea4d5d18fe91beb383f821
xijia <sydenny@126.com>
2020-04-13 19:37:08 +0800

    :feat: Add start-up page

--- android/app/src/main/java/com/find/MainActivity.java
+++ android/app/src/main/java/com/find/MainActivity.java
@@ -4,9 +4,19 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.os.Bundle; // here
+import com.facebook.react.ReactActivity;
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
+// react-native-splash-screen < 0.3.1
+// import com.cboy.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
-
+    // @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this,true);  // here
+        super.onCreate(savedInstanceState);
+    }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
--- index.js
+++ index.js
@@ -2,7 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-AppRegistry.registerComponent(appName, () => App);
+import { name as appName } from './app.json';
+import SplashScreen from 'react-native-splash-screen';
+AppRegistry.registerComponent(appName, () => {
+    SplashScreen.hide();
+    return App;
+});
